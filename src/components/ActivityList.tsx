038d25e05d2f437035a7d4ff25a606bf20d3d8fb
Ariel Hurtado <arielhurtado388@gmail.com>
2024-12-06 18:25:39 -0500

    Edicion de actividades parte 1

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,12 +1,18 @@
+import { useMemo, Dispatch } from "react";
 import { Activity } from "../types";
 import { categories } from "../data/categories";
-import { useMemo } from "react";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { ActivityActions } from "../reducers/activityReducer";
 
 type ActivityListProps = {
   activities: Activity[];
+  dispatch: Dispatch<ActivityActions>;
 };
 
-export default function ActivityList({ activities }: ActivityListProps) {
+export default function ActivityList({
+  activities,
+  dispatch,
+}: ActivityListProps) {
   const categoryName = useMemo(
     () => (category: Activity["category"]) =>
       categories.map((cat) => (cat.id === category ? cat.name : "")),
@@ -38,7 +44,15 @@ export default function ActivityList({ activities }: ActivityListProps) {
             </p>
           </div>
 
-          <div></div>
+          <div className="flex gap-5 items-center">
+            <button
+              onClick={() =>
+                dispatch({ type: "set-activeId", payload: { id: activity.id } })
+              }
+            >
+              <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+            </button>
+          </div>
         </div>
       ))}
     </>
